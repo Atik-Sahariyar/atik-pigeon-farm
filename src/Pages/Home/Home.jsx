7608712051f8fd7e440be6d3e75659902d520a89
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-08-22 12:23:03 +0600

    inital project setup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h3 className=" text-3xl text-blue-700 text-center font-bold mt-16"> Welcome to Atik Pigeon Farm</h3>
+    </div>
+  );
+};
+
+export default Home;
